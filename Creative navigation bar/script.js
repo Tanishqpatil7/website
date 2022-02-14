181da449d97f090d17d905c42c5015aebf45a366
Caskader <siddhvasa@gmail.com>
2022-02-14 16:50:25 +0530

    .

--- Creative navigation bar/script.js
+++ Creative navigation bar/script.js
@@ -14,12 +14,20 @@ function hide() {
         document.getElementById('li2').style.borderRadius = '20px';
         document.getElementById('li3').style.borderRadius = '20px';
         document.getElementById('li4').style.borderRadius = '20px';
+        document.getElementById('1').style.padding = '0';
+        document.getElementById('2').style.padding = '0';
+        document.getElementById('3').style.padding = '0';
+        document.getElementById('4').style.padding = '0';
         document.getElementById('1').innerHTML = ""
         document.getElementById('2').innerHTML = ""
         document.getElementById('3').innerHTML = ""
         document.getElementById('4').innerHTML = ""
     }
     else {
+        document.getElementById('1').style.paddingRight = '5em';
+        document.getElementById('2').style.paddingRight = '5em';
+        document.getElementById('3').style.paddingRight = '5em';
+        document.getElementById('4').style.paddingRight = '5em';
         document.getElementById('1').innerHTML = "Home"
         document.getElementById('2').innerHTML = "Settings"
         document.getElementById('3').innerHTML = "Contact"
